6d368aa8559702488fd0590db6a1a4dd265c4507
james williams & Garrett Olson <student@epicodus.com>
2015-06-02 09:29:47 -0700

    add function for address class and prototype to call full address

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
+function Contact(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName  = lastName;
+  this.addresses = [];
+};
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
+
+function Address(street, city, state) {
+  this.street = street;
+  this.city   = city;
+  this.state  = state;
+}
+
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state
+};
+
 $(function() {
     $("#add-address").click(function() {
         $("#new-addresses").append('<div class="new-address">' +
--- spec/specs.js
+++ spec/specs.js
@@ -1,9 +1,30 @@
-describe("Address", function() {
+describe("Contact", function() {
 
   it("Creates a given contact with specific specifications", function() {
     var newContact = new Contact("jack", "gill");
     expect(newContact.firstName).to.equal("jack");
     expect(newContact.lastName).to.equal("gill");
-    expect(newContact.addresses).to.eql("[]");
+    expect(newContact.addresses).to.eql([]);
   });
-})
+
+  it("adds #fullName for all contacts", function() {
+    var newContact = new Contact("jack", "gill");
+    expect(newContact.fullName()).to.equal("jack gill");
+  });
+});
+
+describe("Address", function() {
+
+  it("Creates a given contact with specific specifications", function() {
+    var newAddress = new Address("123 main st", "test city", "test state");
+    expect(newAddress.street).to.equal("123 main st");
+    expect(newAddress.city).to.equal("test city");
+    expect(newAddress.state).to.eql("test state");
+  });
+
+  it("adds #fullAddress for all addresses", function() {
+    var newAddress = new Address("123 main st", "test city", "test state");
+    expect(newAddress.fullAddress()).to.equal("123 main st, test city, test state");
+  });
+
+});
